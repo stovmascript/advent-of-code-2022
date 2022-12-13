db4567936124b5c56fba49b171fc37ae071a6ef8
Martin Stovicek <martin.stovicek+oss@gmail.com>
2022-12-13 10:10:15 +0100

    Day 1

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -1,6 +1,26 @@
-(ns advent-of-code-2022.core)
+(ns advent-of-code-2022.core
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn sum-calories-by-food [food]
+  (reduce + (map #(Integer/parseInt %) food)))
+
+(def calories-by-elf
+  (->>
+   (io/resource "inputs/day_01")
+   (slurp)
+   (str/split-lines)
+   (partition-by #(= "" %))
+   (remove
+    (fn [items]
+      (and
+       (= (count items) 1)
+       (= "" (apply str items)))))
+   (map sum-calories-by-food)))
+
+;; day 1, part 1
+(apply max calories-by-elf)
+
+;; day 1, part 2
+(reduce + (take-last 3 (sort calories-by-elf)))
