61ea90c054344cd8d869ef8872c3201e14714cc9
Martin Stovicek <martin.stovicek+oss@gmail.com>
2022-12-26 22:15:44 +0100

    Day 2

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -3,13 +3,13 @@
    [clojure.java.io :as io]
    [clojure.string :as str]))
 
+;; Day 1: Calorie Counting
 (defn sum-calories-by-food [food]
   (reduce + (map #(Integer/parseInt %) food)))
 
 (def calories-by-elf
   (->>
-   (io/resource "inputs/day_01")
-   (slurp)
+   (slurp (io/resource "inputs/day_01"))
    (str/split-lines)
    (partition-by #(= "" %))
    (remove
@@ -19,8 +19,65 @@
        (= "" (apply str items)))))
    (map sum-calories-by-food)))
 
-;; day 1, part 1
 (apply max calories-by-elf)
 
-;; day 1, part 2
+;; Day 1: Calorie Counting, part 2
 (reduce + (take-last 3 (sort calories-by-elf)))
+
+;; Day 2: Rock Paper Scissors
+(def day-02-input
+  (slurp (io/resource "inputs/day_02")))
+
+(def shapes-to-outcome
+  {"A X" 4
+   "A Y" 8
+   "A Z" 3
+   "B X" 1
+   "B Y" 5
+   "B Z" 9
+   "C X" 7
+   "C Y" 2
+   "C Z" 6})
+
+(defn score
+  ([] (score day-02-input))
+  ([input]
+   (->>
+    (str/split-lines input)
+    (map
+     (fn [round]
+       (let [shapes round]
+         (shapes-to-outcome (str shapes)))))
+    (reduce +))))
+
+(score)
+
+;; Day 2: Rock Paper Scissors, part 2
+(def shape-to-outcome
+  {"A" 1 "B" 2 "C" 3
+   "X" 0 ; loss
+   "Y" 3 ; draw
+   "Z" 6 ; win
+   })
+
+(def strategy
+  {"A" {"X" "C"
+        "Y" "A"
+        "Z" "B"}
+   "B" {"X" "A"
+        "Y" "B"
+        "Z" "C"}
+   "C" {"X" "B"
+        "Y" "C"
+        "Z" "A"}})
+
+(->>
+ (str/split-lines day-02-input)
+ (map
+  (fn [round]
+    (let [[p1 p2] (str/split round #" ")
+          o2 ((strategy (str p1)) (str p2))]
+      (+
+       (shape-to-outcome (str p2))
+       (shape-to-outcome (str o2))))))
+ (reduce +))
--- test/advent_of_code_2022/core_test.clj
+++ test/advent_of_code_2022/core_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2022.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-score
+  (is
+   (= (score "A X")
+      (shapes-to-outcome (str "A X"))))
+  (is
+   (= (score "A Y")
+      (shapes-to-outcome (str "A Y"))))
+  (is
+   (= (score "A Z")
+      (shapes-to-outcome (str "A Z"))))
+  (is
+   (= (score "B X")
+      (shapes-to-outcome (str "B X"))))
+  (is
+   (= (score "B Y")
+      (shapes-to-outcome (str "B Y"))))
+  (is
+   (= (score "B Z")
+      (shapes-to-outcome (str "B Z"))))
+  (is
+   (= (score "C X")
+      (shapes-to-outcome (str "C X"))))
+  (is
+   (= (score "C Y")
+      (shapes-to-outcome (str "C Y"))))
+  (is
+   (= (score "C Z")
+      (shapes-to-outcome (str "C Z")))))
+
+(test-score)
